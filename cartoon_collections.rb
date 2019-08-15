d604191e48fe51bc67a221b2fd08cf40fcb9e3f8
David Ingram <david@ingramaviation.com>
2019-08-15 19:05:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+   dwarves.each_with_index{|element, index| puts "#{index + 1} #{element}"}
+end
+  
+
+def summon_captain_planet(planeteer_calls)
+   planeteer_calls.map{|element| element.capitalize + '!'}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any?{|element| element.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(contains_cheddar)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  contains_cheddar.find{|element| cheese_types.include?(element)}
 end
